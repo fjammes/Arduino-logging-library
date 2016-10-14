133442249ec0d229f4e863e6147c30cb6572fd06
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-10-14 12:32:47 +0200

    Start platformio integration

---
+++ src/Log.h
@@ -0,0 +1,15 @@
+// Copyright Fabrice Jammes 2016
+// GPLv3 License
+//
+// Arduino Logging library
+// https://github.com/fjammes/ArduinoLogging
+// If you like this project, please add a star!
+
+
+// About this file
+// ---------------
+// This file is here to please the Arduino IDE. It must be present in the src/
+// for the IDE to find it. Feel free to ignore this file if your working in
+// another environment
+
+#include "../include/Log.h"
